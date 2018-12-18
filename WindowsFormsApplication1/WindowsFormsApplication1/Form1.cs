ed3ee7965a60ede9cd1b5903d6ad533d8406f815
Yusuf Sarıyıldız <yusuf1925@users.noreply.github.com>
2018-12-18 15:41:17 +0300

    swapped
    swapped

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                int a;
+                int b;
+                a = Convert.ToInt16(textBox1.Text);
+                b = Convert.ToInt16(textBox2.Text);
+
+                a = a + b;
+                b = a - b;
+                a = a - b;
+                label1.Text = a.ToString();
+                label2.Text = b.ToString();
+                textBox1.Text = a.ToString();
+                textBox2.Text = b.ToString();
+            
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
